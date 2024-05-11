01e9316b3c37231ab51c913659047cb685e18643
Kent Grigo <KentGrigo@hotmail.com>
2024-05-12 00:45:50 +0200

    Make statement top-level and clean up

--- interpreter.ml
+++ interpreter.ml
@@ -1,67 +1,87 @@
-/*
-expr :=
-| ID
-| INT
-| ID = expr
-| expr + expr
-*/
+exception Invalid_storage
 
-alias ValueStorage = Map<(ID, Int)>
-alias FunctionStorage = Map<(ID, Expr)>
+type id = string
 
-data Expr =
-| ID of string
+type expression =
+| Id of id
 | Int of int
-| Assign of ID * Expr
-| Add of Expr * Expr
-| Statement of [Expr]
-| FuncDecl of ID * Expr
-| FuncApp of ID
+| Add of expression * expression
+| FuncApp of id
 
-Statement [
-  (Assign (ID "x") (Int 2))
-  (Assign (ID "y") (Int 3))
-  (Add (ID "x") (ID "y"))
+type statement =
+| Expr of expression
+| Assign of id * expression
+| FuncDecl of id * expression
+
+type program =
+| Program of statement list
+
+(* TODO: Should be hashmap instead of list *)
+type value_storage = (id * int) list
+type function_storage = (id * expression) list
+
+let program = Program [
+  Expr (Id "x");
+  Expr (Int 5);
+  Expr (Add (Int 5, Int 4));
+  Expr (Add (Int 5, Int 4));
+  Assign ("x", Int 2);
+  Assign ("y", Int 3);
+  Expr (Add (Id "x", Id "y"));
 ]
 
-fun interpret (expr: Expr): Int =
-    let (val, _, _) = interpretExpr(expr, [], [])
-    val
+let rec lookup (id: id) (storage: (id * 'a) list): 'a =
+  match storage with
+  | [] -> raise Invalid_storage
+  | (id', element)::storage' ->
+    if String.equal id id'
+    then element
+    else lookup id storage'
 
-fun interpretStatement(
-    exprs: [Expr],
-    valueStorage: ValueStorage,
-    functionStorage: FunctionStorage,
-): (Int, ValueStorage, FunctionStorage) =
-    match exprs with
-    | [] => throw
-    | expr'::[] => interpretExpr(expr', storage)
-    | expr'::expr's =>
-      let (val, storage') = interpretExpr(expr', storage)
-      interpretStatement(expr's, storage')
+let rec interpretExpression
+(expression: expression)
+(valueStorage: value_storage)
+(functionStorage: function_storage)
+: int =
+match expression with
+| Id id -> lookup id valueStorage
+| Int value -> value
+| Add (expression1, expression2) ->
+  let value1: int = interpretExpression expression1 valueStorage functionStorage
+  and value2: int = interpretExpression expression2 valueStorage functionStorage
+  in value1 + value2
+| FuncApp id ->
+  let expression' = lookup id functionStorage
+  in interpretExpression expression' valueStorage functionStorage
 
-fun interpretExpr(
-    expr: Expr,
-    valueStorage: ValueStorage,
-    functionStorage: FunctionStorage,
-): (Int, ValueStorage, FunctionStorage) =
-  match expr with
-  | ID id => (storage[id], valueStorage, functionStorage) // TODO: might not exist
-  | Int val => (val, valueStorage, functionStorage)
-  | Assign (id, expr') =>
-    let (val, valueStorage') = interpretExpr(expr, valueStorage, functionStorage)
-    valueStorage'[id] = val
-    (val, valueStorage', functionStorage)
-  | Add (expr'1, expr'2) =>
-    let (val1, valueStorage', functionStorage') = interpretExpr(expr'1, valueStorage, functionStorage)
-    let (val2, valueStorage'', functionStorage'') = interpretExpr(expr'2, valueStorage', functionStorage')
-    let val' = val1 + val2
-    (val', valueStorage'', functionStorage'')
-  | Statement expr's =>
-    interpretStatement(expr's, storage)
-  | FuncDecl (id, expr') =>
-    functionStorage[id] = expr'
-    (0, valueStorage, functionStorage)
-  | FuncApp id =>
-    let expr' = functionStorage[id]
-    interpretExpr(expr', valueStorage, functionStorage)
+let interpretStatement
+  (statement: statement)
+  (valueStorage: value_storage)
+  (functionStorage: function_storage)
+: (int * value_storage * function_storage) =
+  match statement with
+  | Expr expression ->
+    let value = interpretExpression expression valueStorage functionStorage
+    in (value, valueStorage, functionStorage)
+  | Assign (id, expression) ->
+    let value = interpretExpression expression valueStorage functionStorage
+    in (value, (id, value)::valueStorage, functionStorage)
+  | FuncDecl (id, expression) -> (0, valueStorage, (id, expression)::functionStorage)
+
+let rec interpretStatements
+  (statements: statement list)
+  (valueStorage: value_storage)
+  (functionStorage: function_storage)
+: (int * value_storage * function_storage) =
+  match statements with
+  | [] -> (0, valueStorage, functionStorage) (* TODO: Reconsider how to handle this case *)
+  | statement'::[] -> interpretStatement statement' valueStorage functionStorage
+  | statement'::statements' ->
+    let (_, valueStorage', functionStorage') = interpretStatement statement' valueStorage functionStorage
+    in interpretStatements statements' valueStorage' functionStorage'
+
+let interpretProgram(program: program): int =
+  match program with
+  | Program statements ->
+    let (value, _, _) = interpretStatements statements [] []
+    in value
